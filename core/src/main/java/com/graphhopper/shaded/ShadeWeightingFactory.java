4430df983e644a229f1cbbbda15454c85eea4a3e
Han Nie <nie.han@northeastern.edu>
2024-11-04 02:27:47 -0800

    fixed if condition and encoded value names

--- core/src/main/java/com/graphhopper/shaded/ShadeWeightingFactory.java
+++ core/src/main/java/com/graphhopper/shaded/ShadeWeightingFactory.java
@@ -2,13 +2,13 @@ package com.graphhopper.shaded;
 
 import com.graphhopper.config.Profile;
 import com.graphhopper.routing.DefaultWeightingFactory;
-import com.graphhopper.routing.ev.BooleanEncodedValue;
 import com.graphhopper.routing.util.EncodingManager;
 import com.graphhopper.routing.weighting.Weighting;
 import com.graphhopper.storage.BaseGraph;
 import com.graphhopper.util.PMap;
 
 public class ShadeWeightingFactory extends DefaultWeightingFactory {
+
   private final ShadeDataManager shadeManager;
   private final EncodingManager encodingManager;
 
@@ -21,11 +21,12 @@ public class ShadeWeightingFactory extends DefaultWeightingFactory {
 
   @Override
   public Weighting createWeighting(Profile profile, PMap hints, boolean disableTurnCosts) {
-    if ("shaded".equalsIgnoreCase(profile.getName())){
+    if (!"shaded".equalsIgnoreCase(profile.getName())) {
       throw new IllegalArgumentException("Only shaded profile is supported");
     }
     ShortestWeighting shortestWeighting = new ShortestWeighting(
-        encodingManager.getBooleanEncodedValue("access"), encodingManager.getDecimalEncodedValue("speed"));
-    return new ShadedWeighting( shortestWeighting, shadeManager);
+        encodingManager.getBooleanEncodedValue("car_access"),
+        encodingManager.getDecimalEncodedValue("car_average_speed"));
+    return new ShadedWeighting(shortestWeighting, shadeManager);
   }
 }
