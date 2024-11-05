9e18f70a9875c695d2073037384f127aef049a20
Han Nie <nie.han@northeastern.edu>
2024-11-04 20:59:02 -0800

    fixed the subnetwork issue by adding graph status and returning weight as normal when graph has not been cleaned up

--- core/src/main/java/com/graphhopper/shaded/ShadedWeighting.java
+++ core/src/main/java/com/graphhopper/shaded/ShadedWeighting.java
@@ -8,14 +8,19 @@ public class ShadedWeighting extends AbstractAdjustedWeighting {
 
   private final ShadeDataManager shadeManager;
   private static final String name = "shaded";
+  private final GraphStatus graphStatus;
 
   public ShadedWeighting(Weighting superWeighting, ShadeDataManager shadeDataManager) {
     super(superWeighting);
     this.shadeManager = shadeDataManager;
+    this.graphStatus = new GraphStatus();
   }
 
   @Override
   public double calcEdgeWeight(EdgeIteratorState edgeState, boolean reverse) {
+    if (!graphStatus.getCleanedUp()) {
+      return superWeighting.calcEdgeWeight(edgeState, reverse);
+    }
     if (!shadeManager.withinRange(edgeState)) {
       return Double.POSITIVE_INFINITY;
     }
